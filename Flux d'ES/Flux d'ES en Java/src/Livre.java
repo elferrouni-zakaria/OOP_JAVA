673d17f93435033e5790ed0dd0ac9389632a4249
EL FERROUNI ZAKARIA <zakariaelferrouni@gmail.com>
2024-11-18 00:00:38 +0100

    ajouter Flux d'ES

---
+++ Flux d'ES/Flux d'ES en Java/src/Livre.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+public class Livre implements Serializable {
+    private String titre;
+    private String auteur;
+    private int anneePublication;
+
+    public Livre(String titre, String auteur, int anneePublication) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.anneePublication = anneePublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+                "titre='" + titre + '\'' +
+                ", auteur='" + auteur + '\'' +
+                ", anneePublication=" + anneePublication +
+                '}';
+    }
+}
---
+++ Flux d'ES/Flux d'ES en Java/src/SerialisationBibliotheque.java
@@ -0,0 +1,19 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerialisationBibliotheque {
+    public static void main(String[] args) {
+        List<Livre> livres = new ArrayList<>();
+        livres.add(new Livre("Le Petit Prince", "Antoine de Saint-Exupéry", 1943));
+        livres.add(new Livre("1984", "George Orwell", 1949));
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("bibliotheque.ser"))) {
+            oos.writeObject(livres); // Sauvegarde la liste d'objets
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
